5bc2d7ede248776e66d92edc96625efe3d5ad049
Bryan Rodriguez <Serphp@hotmail.com>
2022-10-17 06:47:11 -0500

    add models->notes.php, composer extras and update schema.sql

---
+++ src/models/Notes.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Serph\Notas\models;
+use Serph\Notas\lib\Database;
+
+class Note extends Database{
+    private string $uuid; //llamo uuid de la clase Database
+    public function __construct(private string $title, private string $content)
+    {//llama constructor padre
+        parent::__construct();
+        $this->uuid = uniqid();
+    }
+
+    public function save(){ //guarda en mi base datos
+        $query = $this->connect()->prepare('INSERT INTO notes (uuid, title, content) VALUES (:uuid, :title, :content, NOW())');
+        $query->execute(['
+        uuid' => $this->uuid, 
+        'title' => $this->title, 
+        'content' => $this->content]);
+    }
+
+    public function update(){ //actualiza mi base de datos
+        $query = $this->connect()->prepare('UPDATE notes SET title = :title, content = :content WHERE uuid = :uuid');
+        $query->execute(['
+        uuid' => $this->uuid, 
+        'title' => $this->title, 
+        'content' => $this->content]);
+    }   
+
+    public static function get($uuid){
+        $db = new Database();
+        $query = $db->connect()->prepare('SELECT * FROM notes WHERE uuid = :uuid');
+        $query->execute(['uuid' => $uuid]);
+    }
+
+    public function getUUID(){
+        return $this->uuid;
+    }
+    public function setUUID($value){
+        $this->uuid = $value;
+    }
+    public function setTitle($value){
+        $this->title = $value;
+    }
+    public function getContent(): string{
+        return $this->content;
+    }
+    public function setContent($value){
+        $this->content = $value;
+    }
+
+}
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'serph/notas',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cd8830cb843038e55cf3b8340da4fca50b7c56b6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'serph/notas' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cd8830cb843038e55cf3b8340da4fca50b7c56b6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
